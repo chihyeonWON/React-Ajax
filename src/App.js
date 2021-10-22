5bc99d195efb6c9d93b831fa4d10567d90544402
원치현 <heungeob1003@gmail.com>
2021-10-22 16:28:20 +0900

    Import React Component and create Nav Component and apply this to App Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return(
-    <div className="App">
-      <h1>WEB</h1>
+class Nav extends Component {
+  render() {
+    return(
       <nav>
         <li><a href="1">HTML</a></li>
         <li><a href="2">CSS</a></li>
         <li><a href="3">JS</a></li>
       </nav>
+    );
+  }
+}
+
+function App() {
+  return(
+    <div className="App">
+      <h1>WEB</h1>
+      <Nav></Nav>
       <article>
         <h2>Welcome</h2>
         Hello, React &amp; Ajax
