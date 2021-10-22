a6f9ace28f9d2de22a1b511c5c174e400cc099f4
원치현 <heungeob1003@gmail.com>
2021-10-22 16:43:34 +0900

    Create Nav Component's list by using javascript objects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 class Nav extends Component {
+  state = {
+    list:[]
+  }
   componentDidMount(){
     fetch('list.json')
     .then(function(result){ 
@@ -8,14 +11,18 @@ class Nav extends Component {
     })
     .then(function(json) {
       console.log(json);
+      this.setState({ list:json });
     }.bind(this));
   }
   render() {
+    var listTag = [];
+    for(var i=0; i<this.state.list.length; i++){
+      var li = this.state.list[i];
+      listTag.push(<li key={li.id}><a href={li.id}>{li.title}</a></li>);
+    }
     return(
       <nav>
-        <li><a href="1">HTML</a></li>
-        <li><a href="2">CSS</a></li>
-        <li><a href="3">JS</a></li>
+        {listTag}
       </nav>
     );
   }
